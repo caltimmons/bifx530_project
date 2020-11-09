94d1dc6cdb7304df86c6e8d5e2eb5f19c808bc40
caltimmons <40839829+caltimmons@users.noreply.github.com>
2020-11-08 19:23:03 -0500

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,3 @@
+insert into employees(employee_id, name, department_id, team_id) values(30000000,”Carrie Fisher”,1,1);
+insert into login(login_id, password) values(”cfisher”,”password1”);
+insert into employee_login(login_id, employee_id) values(30000000,”cfisher”);
