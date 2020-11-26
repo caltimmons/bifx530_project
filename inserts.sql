59edeac11f3c336c70bbedbd7f0da31c83bb21a0
caltimmons <40839829+caltimmons@users.noreply.github.com>
2020-11-25 20:21:27 -0500

    Add files via upload

--- inserts.sql
+++ inserts.sql
@@ -1,5 +1,5 @@
-insert into employee(employee_id, name, department_id, team_id) values(30000000,”Carrie Fisher”,1,1);
-insert into employee(employee_id, name, department_id, team_id) values(30000001,”Harrison Ford”,3,1);
+insert into employee(employee_id, name, department_id, team_id) values(30000000,"Carrie Fisher",1,null);
+insert into employee(employee_id, name, department_id, team_id) values(30000001,"Harrison Ford",3,1);
 insert into employee(employee_id, name, department_id, team_id) values(30000002,”John Boyega”,9,1);
 insert into employee(employee_id, name, department_id, team_id) values(30000003,”Daisy Ridley”,7,2);
 insert into employee(employee_id, name, department_id, team_id) values(30000004,”Adam Driver”,5,2);
@@ -17,9 +17,9 @@ insert into employee(employee_id, name, department_id, team_id) values(20000006,
 insert into employee(employee_id, name, department_id, team_id) values(20000007,”Kathy Taylor”,8,10);
 insert into employee(employee_id, name, department_id, team_id) values(20000008,”Tina Brown”,5,6);
 insert into employee(employee_id, name, department_id, team_id) values(20000009,”Courtney Jones”,2,6);
-insert into employee(employee_id, name, department_id, team_id) values(20000010,”Mary Miller”,10,5);
+insert into employee(employee_id, name, department_id, team_id) values(20000010,”Mary Miller”,10,);
 
-insert into login(login_id, password) values(”cfisher”,”password1”);
+insert into login(login_id, employee_id, password) values("cfisher",30000000, "password1");
 insert into login(login_id, password) values(”hford”,”password2”);
 insert into login(login_id, password) values(”jboyega”,”password3”);
 insert into login(login_id, password) values(”dridley”,”password4”);
@@ -40,28 +40,17 @@ insert into login(login_id, password) values(”Tbrown”,”password18”);
 insert into login(login_id, password) values(”Cjones”,”password19”);
 insert into login(login_id, password) values(”Mmiller”,”password20”);
 
-insert into team(team_id, team_name, employee_id) values(1,”Metro”,30000000);
-insert into team(team_id, team_name, employee_id) values(1,”Metro”,30000001);
-insert into team(team_id, team_name, employee_id) values(1,”Metro”,30000002);
+insert into team(team_id, team_name, employee_id) values(1,"Metro",30000000);
 insert into team(team_id, team_name, employee_id) values(2,”Liberty”,30000003);
-insert into team(team_id, team_name, employee_id) values(2,”Liberty”,30000004);
-insert into team(team_id, team_name, employee_id) values(2,”Liberty”,30000005);
 insert into team(team_id, team_name, employee_id) values(3,”Classic”,30000006);
-insert into team(team_id, team_name, employee_id) values(3,”Classic”,30000007);
-insert into team(team_id, team_name, employee_id) values(3,”Classic”,30000008);
 insert into team(team_id, team_name, employee_id) values(4,”Rock”,30000009);
-insert into team(team_id, team_name, employee_id) values(1,”Metro”,20000001);
 insert into team(team_id, team_name, employee_id) values(5,”Star”,20000002);
 insert into team(team_id, team_name, employee_id) values(6,”Trump”,20000003);
 insert into team(team_id, team_name, employee_id) values(7,”Courageous”,20000004);
 insert into team(team_id, team_name, employee_id) values(8,”Winner”,20000005);
-insert into team(team_id, team_name, employee_id) values(2,”Liberty”,20000006);
 insert into team(team_id, team_name, employee_id) values(10,”Joe Biden”,20000007);
-insert into team(team_id, team_name, employee_id) values(6,”Trump”,20000008);
-insert into team(team_id, team_name, employee_id) values(6,”Trump”,20000009);
-insert into team(team_id, team_name, employee_id) values(5,”Star”,20000010);
 
-insert into reserves(login_id, employee_id, building_no, room_no, meeting_date, meeting_type, start_time, end_time) values(”cfisher”,30000000,100,1,110920,”Training”,1000,1130);
+insert into reserves(login_id, employee_id, building_no, room_no, meeting_date, meeting_type, start_time, end_time) values("cfisher",30000000,100,1,'1990-09-01',"Training",'10:30:00','11:30:00');
 insert into reserves(login_id, employee_id, building_no, room_no, meeting_date, meeting_type, start_time, end_time) values(”hford”,30000001,100,1,111120,”Training”,1400,1530);
 insert into reserves(login_id, employee_id, building_no, room_no, meeting_date, meeting_type, start_time, end_time) values(”jboyega”,30000002,100,2,112520,”Brainstorming”,0900,1100);
 insert into reserves(login_id, employee_id, building_no, room_no, meeting_date, meeting_type, start_time, end_time) values(”dridley”,30000003,100,2,120320,”Tier Meeting”,0930,1030);
@@ -72,7 +61,7 @@ insert into reserves(login_id, employee_id, building_no, room_no, meeting_date, 
 insert into reserves(login_id, employee_id, building_no, room_no, meeting_date, meeting_type, start_time, end_time) values(”msmith”,30000008,100,4,122320,”Brainstorming”,1400,1500);
 insert into reserves(login_id, employee_id, building_no, room_no, meeting_date, meeting_type, start_time, end_time) values(”jolson”,30000009,100,4,122120,”Training”,1000,1130);
 
-insert into department(department_id, department_name) values(1,”Accounting”);
+insert into department(department_id, department_name) values(1,"Accounting");
 insert into department(department_id, department_name) values(2,”Sales”);
 insert into department(department_id, department_name) values(3,”Quality Control”);
 insert into department(department_id, department_name) values(4,”Human Resources”);
@@ -83,7 +72,7 @@ insert into department(department_id, department_name) values(8,”Manufacturing
 insert into department(department_id, department_name) values(9,”Production”);
 insert into department(department_id, department_name) values(10,”Lab Services”);
 
-insert into building(building_no, building_name, location) values(100,”Pembro”,”Frederick”);
+insert into building(building_no, building_name, location) values(100,"Pembro","Frederick");
 insert into building(building_no, building_name, location) values(101,”Tralo”,”Frederick”);
 insert into building(building_no, building_name, location) values(102,”Durva”,”Frederick”);
 insert into building(building_no, building_name, location) values(103,”R&D”,”Walkersville”);
@@ -94,7 +83,7 @@ insert into building(building_no, building_name, location) values(201,”COVID T
 insert into building(building_no, building_name, location) values(203,”STAT Testing”,”Urbana”);
 insert into building(building_no, building_name, location) values(303,”Purification”,”Walkersville”);
 
-insert into room(room_no, building_no, capacity, av_equipment) values(1,100,10,”Audio”);
+insert into room(room_no, building_no, capacity, av_equipment) values(1,100,10,"Audio");
 insert into room(room_no, building_no, capacity, av_equipment) values(2,100,20,”Audio”);
 insert into room(room_no, building_no, capacity, av_equipment) values(3,100,50,”Audio”);
 insert into room(room_no, building_no, capacity, av_equipment) values(4,100,96,”Audio and Video”);
